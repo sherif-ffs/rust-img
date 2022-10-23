bbccc86661db73fcf2300c4fb8455aa90e2c1c8c
Sherif Elmetwally <sge5@njit.edu>
2022-10-23 19:11:34 -0400

    idk

--- rust-img/src/main.rs
+++ rust-img/src/main.rs
@@ -2,13 +2,13 @@ use image::{GenericImageView, ImageBuffer, Pixel, Rgba, DynamicImage};
 use rand::Rng; // 0.8.5
 
 fn main() {
-  let img = image::open("./smok.jpg").expect("File not found!");
+  let img = image::open("./grad.jpg").expect("File not found!");
   let (w, h) = img.dimensions();
   let mut output = ImageBuffer::new(w, h); // create a new buffer for our output
   let (w, h) = img.dimensions();
   
   let mut blur = |x, y| {
-    let test_rand = rand::thread_rng().gen_range(1..250);
+    let test_rand = rand::thread_rng().gen_range(1..50);
     let new_x = if x+test_rand < w { x+test_rand } else { 0 };
     let new_y = if y+test_rand < h { y+test_rand } else { 0 };
   
@@ -32,16 +32,7 @@ fn main() {
   }
   
   // red
-  // for (x, y, pixel) in img.pixels() {
-  //   let rand_r= rand::thread_rng().gen_range(180..210);
-  //   let rand_g= rand::thread_rng().gen_range(0..10);
-  //   let rand_b= rand::thread_rng().gen_range(10..40);
-  //   let new_pixel = image::Rgba([rand_r, rand_g, rand_b, 255]);
 
-  //   if x % 2 == 0 && y % 2 != 0 {
-  //     output.put_pixel(x, y, new_pixel);
-  //   }
-  // }
 
-  output.save("output4.png").unwrap();
+  output.save("output8.png").unwrap();
 }
